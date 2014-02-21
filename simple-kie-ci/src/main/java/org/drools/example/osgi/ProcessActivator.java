8b69f48d037a3b00e60acbd462133a7929020918
Charles Moulliard <ch007m@gmail.com>
2014-02-21 19:08:10 +0100

    Add doc, change order of packages to be deployed, modify drl

--- simple-kie-ci/src/main/java/org/drools/example/osgi/ProcessActivator.java
+++ simple-kie-ci/src/main/java/org/drools/example/osgi/ProcessActivator.java
@@ -17,18 +17,20 @@ public class ProcessActivator implements BundleActivator {
     public void start(BundleContext bundleContext) throws Exception {
         KieServices kieServices = KieServices.Factory.get();
 
-        ReleaseId releaseId = kieServices.newReleaseId( "org.test", "kie-project-simple", "1.4" );
+        ReleaseId releaseId = kieServices.newReleaseId( "org.test", "kie-project-simple", "1.0" );
 
         KieContainer kContainer = kieServices.newKieContainer( releaseId );
 
         kScanner = kieServices.newKieScanner( kContainer );
+
+        System.out.println("This is a Kie-Ci example. The drl rule is packaged as a kmodule in a jar and deployed in your maven repo");
 
         for (int i = 0; i < 100; i++) {
 
             kScanner.scanNow();
 
             ksession = kContainer.newKieSession();
-            ksession.insert("test");
+            ksession.insert("Hello");
             ksession.fireAllRules();
 
             Thread.sleep(5000);

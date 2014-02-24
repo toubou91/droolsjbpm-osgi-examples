55f2d95b2f3d092bee25aee24e1792619d333dea
Charles Moulliard <ch007m@gmail.com>
2014-02-24 10:56:08 +0100

    Refactor version number, add try/catch block

--- simple-kie-ci/src/main/java/org/drools/example/osgi/ProcessActivator.java
+++ simple-kie-ci/src/main/java/org/drools/example/osgi/ProcessActivator.java
@@ -8,36 +8,42 @@ import org.kie.api.runtime.KieSession;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import java.util.NoSuchElementException;
+
 public class ProcessActivator implements BundleActivator {
 
     private KieScanner kScanner;
-    private KieSession ksession;
+    private KieSession kSession;
 
     @Override
-    public void start(BundleContext bundleContext) throws Exception {
-        KieServices kieServices = KieServices.Factory.get();
-
-        ReleaseId releaseId = kieServices.newReleaseId( "org.test", "kie-project-simple", "1.0" );
-
-        KieContainer kContainer = kieServices.newKieContainer( releaseId );
-
-        kScanner = kieServices.newKieScanner( kContainer );
-
-        System.out.println("This is a Kie-Ci example. The drl rule is packaged as a kmodule in a jar and deployed in your maven repo");
-
+    public void start(BundleContext bundleContext) {
         try {
+            KieServices kieServices = KieServices.Factory.get();
+
+            ReleaseId releaseId = kieServices.newReleaseId("org.test", "kie-project-simple", "1.0");
+
+            KieContainer kContainer = kieServices.newKieContainer(releaseId);
+
+            kScanner = kieServices.newKieScanner(kContainer);
+
+            System.out.println("This is a Kie-Ci example. The drl rule is packaged as a kmodule in a jar and deployed in your maven repo");
+
             for (int i = 0; i < 100; i++) {
 
                 kScanner.scanNow();
 
-                ksession = kContainer.newKieSession();
-                ksession.insert("Hello");
-                ksession.fireAllRules();
+                kSession = kContainer.newKieSession();
+                kSession.insert("Hello");
+                kSession.fireAllRules();
 
                 Thread.sleep(10000);
             }
         } catch (InterruptedException e) {
             System.out.println("Ctrl-c command executed. Process interrupted");
+        } catch (NoSuchElementException ne) {
+            ne.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -45,10 +51,10 @@ public class ProcessActivator implements BundleActivator {
     public void stop(BundleContext bundleContext) throws Exception {
         if (this.kScanner != null) {
             this.kScanner.stop();
-            System.out.println("KieSscanner stopped.");
+            System.out.println("KieScanner stopped.");
         }
-        if (this.ksession != null) {
-            this.ksession.dispose();
+        if (this.kSession != null) {
+            this.kSession.dispose();
             System.out.println("KieSession disposed.");
         }
     }
